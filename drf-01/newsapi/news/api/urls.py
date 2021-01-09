a350de1fe582dba6f954c59f46853888749c1c87
Greg <provo17.gp@gmail.com>
2021-01-08 20:56:49 -0600

    Class based apiview created

---
+++ drf-01/newsapi/news/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from news.api.views import (ArticleDetailAPIView, ArticleListCreateAPIView)
+
+urlpatterns = [
+    path("articles/",
+         ArticleListCreateAPIView.as_view(),
+         name="article-list"),
+
+    path("articles/<int:pk>/",
+         ArticleDetailAPIView.as_view(),
+         name="article-detail"),
+]
