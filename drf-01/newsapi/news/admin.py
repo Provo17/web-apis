28a6c14dee2ae5d01ff8ad6008ae24b7a7797f20
Greg <provo17.gp@gmail.com>
2021-01-08 23:03:32 -0600

    created jobs api

--- drf-01/newsapi/news/admin.py
+++ drf-01/newsapi/news/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from news.models import Article, Journalist
+from jobsapi.models import JobOffer
 
 admin.site.register(Article)
 admin.site.register(Journalist)
+admin.site.register(JobOffer)
