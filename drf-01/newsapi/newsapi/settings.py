657e013f69ac19849c1eb0c5cfa1f257230d33da
Gregory Provo <provo17.gp@gmail.com>
2021-01-12 22:50:23 -0600

    rest_auth

--- drf-01/newsapi/newsapi/settings.py
+++ drf-01/newsapi/newsapi/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
     'news',
     'jobsapi',
     'ebooksapi',
@@ -130,8 +132,9 @@ MEDIA_ROOT = "uploads"
 
 
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': (
-#         'rest_framework.permissions.IsAuthenticated',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
--- drf-01/newsapi/newsapi/urls.py
+++ drf-01/newsapi/newsapi/urls.py
@@ -18,9 +18,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include("rest_framework.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/rest-auth/", include("rest_auth.urls")),
+    # path("api/rest-auth/registration/", include("rest_auth.registration.urls")),    
     path("api/", include("news.api.urls")),
     path("api/", include("jobsapi.api.urls")),
+    # path("api/", include("profilesapi.api.urls")),
     path("api/", include("ebooksapi.api.urls"))
 ]
 
